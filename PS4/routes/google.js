e53141436ed19ad1c021991eaf27508c54363b7f
Ashley Deshields <adeshiel@bu.edu>
2019-11-17 20:02:55 -0500

    last minute push

--- PS4/routes/google.js
+++ PS4/routes/google.js
@@ -1,6 +1,4 @@
 var express = require('express');
-var hal = require('hal');
-var fs = require('fs');
 const request = require('request')
 var router = express.Router();
 const {OAuth2Client} = require('google-auth-library');
@@ -10,16 +8,16 @@ router.use(express.urlencoded({ extended: false }));
 const oauth = require('../KEY.js');
 const private = require('../private.json')
 const secret = require('../secret.json')
+const loadGoogleMapsApi = require('load-google-maps-api')
 
 const googleMapsClient = require('@google/maps').createClient({
   key: oauth.api_key
 });
-const talent = require('@google-cloud/talent').v4beta1;
 
 
 router.post('/search', async (req, res, next) => {
   try {
-    const ret = await basicLocationSearch(req.body, res);
+    const ret = await findEvents(req.body, res);
 
   } catch(e) {
     res.render('location', {data: e})
@@ -28,13 +26,16 @@ router.post('/search', async (req, res, next) => {
 });
 
 
-const basicLocationSearch = async (body, response) => {
+const findEvents = async (body, response) => {
   try {
 
       let url_option = 'https://app.ticketmaster.com/discovery/v2/events.json?apikey=' + oauth.ticket_key + "&keyword=" + body.event
-      if(body.city) { url_option =  url_option+"&city="+body.city}
+      if(body.city){ url_option =  url_option+"&city="+body.city}
       if(body.country){ url_option =  url_option+"&countryCode="+body.country}
       if(body.state){ url_option =  url_option+"&stateCode="+body.state}
+
+      console.log(url_option)
+
       let result = await request({
         method:'GET',
         url: url_option ,
@@ -43,6 +44,10 @@ const basicLocationSearch = async (body, response) => {
         }}, function(err, res, body){
           const parsed_body = JSON.parse(body);
           const emb = parsed_body._embedded.events
+          let map_points = []
+          // for(const show in emb){
+          //   show
+          // }
           let goodies = JSON.stringify(emb)
           response.render('location', {data: goodies});
         })
